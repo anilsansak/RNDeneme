14d0dfa84b1090cfde910d57faf35e5d89c6f959
anilsansak <anilsansak@gmail.com>
2017-06-29 10:37:43 +0300

    First commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, View,TextInput,Alert,Button } from 'react-native';
+
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {inputText: ''};
+  }
+  _onPressButton(){
+    Alert.alert('Your data has been submitted to the system!')
+  }
+
+  render() {
+    return (
+
+      <View style={ {padding: 100} }>
+
+        <Text style={styles.textstil}>Welcome to BUDGETPACK</Text>
+
+        <TextInput style={ {height: 40} }
+        placeholder="Please enter your budget"
+        onChangeText={ (inputText)=>this.setState( {inputText} ) } />
+
+        <Text style={ {padding:10,fontSize:16} } >
+        Your budget is:{this.state.inputText}
+        </Text>
+
+        <Button
+          onPress={this._onPressButton}
+          title="Submit" />
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textstil: {
+    fontSize: 26,
+    color: 'red',
+  },
+});
