ded5a3775f5e563c32789b4196bcd96eac445434
anilsansak <anilsansak@gmail.com>
2017-06-30 14:16:23 +0300

    added react-navigation

--- App.js
+++ App.js
@@ -1,40 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View,TextInput,Alert,Button } from 'react-native';
+import { StackNavigator,withNavigation} from 'react-navigation';
 
-
-export default class App extends React.Component {
+export default class myApp extends React.Component {
   constructor(props){
     super(props);
-    this.state = {inputText: ''};
+    this.state = {};
+    console.log("deneme1");
   }
-  _onPressButton(){
-    Alert.alert('Your data has been submitted to the system!')
-  }
+  static navigationOptions = {
+    title: 'Welcome',
 
+  };
   render() {
+    console.log("navigatepre");
+    const { navigate } = this.props.navigation;
+    console.log("navigate");
     return (
-
-      <View style={ {padding: 100} }>
-
-        <Text style={styles.textstil}>Welcome to BUDGETPACK</Text>
-
-        <TextInput style={ {height: 40} }
-        placeholder="Please enter your budget"
-        onChangeText={ (inputText)=>this.setState( {inputText} ) } />
-
-        <Text style={ {padding:10,fontSize:16} } >
-        Your budget is:{this.state.inputText}
-        </Text>
-
+      <View>
+        <Text>Hello, Chat App!</Text>
         <Button
-          onPress={this._onPressButton}
-          title="Submit" />
-
+          onPress={() => navigate('Chat', {user: 'Lucy'})}
+          title="Chat with Lucy"
+        />
       </View>
     );
   }
 }
-
+class ChatScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: `Chat with ${navigation.state.params.user}`,
+  });
+  render() {
+    // The screen's current route is passed in to `props.navigation.state`:
+    const { params } = this.props.navigation.state;
+    return (
+      <View>
+        <Text>Chat with {params.user}</Text>
+      </View>
+    );
+  }
+}
+const App = StackNavigator( {
+  Home: {screen: myApp },
+  Chat: { screen: ChatScreen },
+});
 const styles = StyleSheet.create({
   textstil: {
     fontSize: 26,
