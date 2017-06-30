c2e5bcdbe7e51b6e36e42ee6c2c8ae6aed2db63b
anilsansak <anilsansak@gmail.com>
2017-06-30 23:49:06 +0300

    example navigation

--- App.js
+++ App.js
@@ -1,53 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View,TextInput,Alert,Button } from 'react-native';
-import { StackNavigator,withNavigation} from 'react-navigation';
 
-export default class myApp extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {};
-    console.log("deneme1");
-  }
-  static navigationOptions = {
-    title: 'Welcome',
+import React, { Component } from 'react';
+import { Text, View, Button, StyleSheet } from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
-  };
+const Hello = ({ screen, goto, navigate }) => (
+  <View style={styles.container}>
+    <Text style={styles.paragraph}>Hello {screen}</Text>
+    <Button title={'Go to ' + goto} onPress={() => navigate(goto)} />
+  </View>
+)
+
+class ExampleScreenA extends Component {
+  static navigationOptions = { title: "ExampleScreenA" };
+  render() { return (<Hello screen='ExampleScreenA' goto='ScreenB' navigate={this.props.navigation.navigate} />) }
+}
+class ExampleScreenB extends Component {
+  static navigationOptions = { title: "ExampleScreenB" };
+  render() { return (<Hello screen='ExampleScreenB' goto='ScreenA' navigate={this.props.navigation.navigate} />) }
+}
+
+export const StackHome = StackNavigator({
+  ScreenA: { screen: ExampleScreenA },
+  ScreenB: { screen: ExampleScreenB },
+}, {headerMode: 'screen'});
+
+
+
+export default class App extends Component {
   render() {
-    console.log("navigatepre");
-    const { navigate } = this.props.navigation;
-    console.log("navigate");
     return (
-      <View>
-        <Text>Hello, Chat App!</Text>
-        <Button
-          onPress={() => navigate('Chat', {user: 'Lucy'})}
-          title="Chat with Lucy"
-        />
-      </View>
+      <StackHome />
     );
   }
 }
-class ChatScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => ({
-    title: `Chat with ${navigation.state.params.user}`,
-  });
-  render() {
-    // The screen's current route is passed in to `props.navigation.state`:
-    const { params } = this.props.navigation.state;
-    return (
-      <View>
-        <Text>Chat with {params.user}</Text>
-      </View>
-    );
-  }
-}
-const App = StackNavigator( {
-  Home: {screen: myApp },
-  Chat: { screen: ChatScreen },
-});
+
 const styles = StyleSheet.create({
-  textstil: {
-    fontSize: 26,
-    color: 'red',
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 30,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
   },
 });
